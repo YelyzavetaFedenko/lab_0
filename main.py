1deddd245cf3a567d3babf2e4df8c2bd18c2e616
“YelyzavetaFedenko” <yelyzaveta.fedenko@hebron-academy.com>
2023-10-06 13:34:03 +0300

    Solution

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
-name =
-age =
-city =
-country =
+name = "Elizabeth"
+age = "19"
+city = "Krivyi Rih"
+country = "Ukraine"
 
 print(f'My name is {name}')
+print(f'I am {age} years old')
+print(f'I am from {city}, {country}')
